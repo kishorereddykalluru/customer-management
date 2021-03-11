e78ddbbd07609709ee659032d8ddd70445699535
Kishore Reddy <kishorereddykalluru@gmail.com>
2021-03-11 16:37:49 +0530

    swagger implementation, spring boot thread pool task executor and @Async

--- src/main/java/com/customermanagement/controller/CustomerController.java
+++ src/main/java/com/customermanagement/controller/CustomerController.java
@@ -8,6 +8,8 @@ import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.tags.Tags;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +20,7 @@ import java.util.List;
 
 @RestController
 @Slf4j
+@Tag(name = "Customer Management")
 public class CustomerController {
 
     @Autowired
@@ -28,7 +31,15 @@ public class CustomerController {
      *
      * @return CustomerDetails List
      */
-    @GetMapping(value = "/customers/getAll")
+    @GetMapping(value = "${customer-management.getAll}")
+    @Operation(operationId = "getAllCustomers", description = "Customers Management", summary = "Get All Customers",
+            responses =  {
+                    @ApiResponse(responseCode = "200", description = "successful response received", content = @Content(schema = @Schema(implementation = CustomerDetails.class))),
+                    @ApiResponse(responseCode = "400", description = "Not Found error"),
+                    @ApiResponse(responseCode = "406", description = "Not Acceptable Error"),
+                    @ApiResponse(responseCode = "500", description = "Internale Server error")
+
+            })
     public List<CustomerDetails> getAllCustomers(){
         return customerService.getAllCustomers().orElseThrow(() ->new RuntimeException("Customers not found"));
     }
@@ -48,7 +59,7 @@ public class CustomerController {
                         @ApiResponse(responseCode = "500", description = "Internale Server error")
 
                 })
-    @GetMapping(value = "/customers")
+    @GetMapping(value = "${customer-management.findById}")
     public List<CustomerDetails> findByCustomerId(
             @Parameter(name = "id", in = ParameterIn.QUERY, description = "Customer Id")
             @RequestParam("id") List<Long> ids){
