9c9e44cfeb6b941ce8255db424332a55810a77b5
Kishore Reddy <kishorereddykalluru@gmail.com>
2021-04-17 23:29:08 +0530

    new controller

---
+++ src/main/java/com/customermanagement/controller/ItemController.java
@@ -0,0 +1,13 @@
+package com.customermanagement.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ItemController {
+
+    @GetMapping("/item")
+    public String getItem(){
+        return "Item Created";
+    }
+}
